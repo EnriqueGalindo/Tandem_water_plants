cc5c508ab4be0b62b42ae1d33e82d2da8372b522
Enrique Galindo <egalindo@protonmail.com>
2019-12-14 11:00:34 -0500

    Added second test to check if the plants are being watered within the correct time intervals

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -17,3 +17,26 @@ class TestFunctions(unittest.TestCase):
             for date in sorted(plant['schedule']):
                 if date.weekday() == 6 or date.weekday() == 5:
                     self.assertRaises(TypeError)
+
+    def test_timedeltas(self):
+        '''checks to make sure that the distance between waterings for any given plant
+        is no more or less than one day from its desired watering interval'''
+        start_date = datetime.datetime.strptime("12-16-2019", '%m-%d-%Y')
+        weeks = 12
+        old_day
+        data = watering_schedule.get_data("plant_info.json")
+        plant_array = watering_schedule.water2int(data)
+        plant_w_schedule = watering_schedule.schedule_per_plant(start_date,
+                                                                weeks,
+                                                                plant_array)
+        for plant in plant_w_schedule:
+            while sorted(plant['schedule']):
+                new_day = sorted(plant['schedule']).pop()
+                if old_day == 0:
+                    old_day = new_day
+                    continue
+                self.assertTrue(new_day - old_day >= datetime.timedelta(
+                                days=int(plant['water_after']) - 1)
+                self.assertTrue(new_day - old_day <= datetime.timedelta(
+                                days=int(plant['water_after']) + 1)
+                old_day = new_day
